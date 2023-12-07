db7234e42bba6253a26f9fda567e3f7383fb8385
Rohan Reddy <rohanreddy888@gmail.com>
2023-12-08 00:10:05 +0530

    first commit

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-center items-center py-4">
+      <p>All Rights Reserved © 2023 - Build with ❤️ at ETHIndia</p>
+    </footer>
+  );
+}
---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import { usePathname } from "next/navigation";
+
+const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const pathname = usePathname();
+  //navbar scroll changeBackground function
+  const changeBackground = () => {
+    if (window.scrollY >= 10) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  useEffect(() => {
+    setOpen(false);
+  }, [pathname]);
+
+  useEffect(() => {
+    changeBackground();
+    // adding the event when scroll change Background
+    window.addEventListener("scroll", changeBackground);
+  }, [navbar]);
+  return (
+    <>
+      <div className="w-full">
+        <div
+          className={
+            navbar
+              ? "nav-scroll bg-secondary fixed z-50 flex w-full flex-col items-center justify-between px-6 py-4 shadow-md lg:px-24"
+              : "fixed z-20 flex w-full flex-col items-center justify-between px-6 py-4 lg:px-24"
+          }
+        >
+          <div className="flex flex-row justify-between items-center w-full">
+            <Link
+              className="flex flex-row justify-start items-center gap-2"
+              href={"/"}
+            >
+              <Image
+                src={"/logo/TokenKrafters-Teal.png"}
+                alt="TokenKrafters Logo"
+                height={"250"}
+                width={"250"}
+              />
+            </Link>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Navbar;
